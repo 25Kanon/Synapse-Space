8cbbb575d410a68e05a233449c9b9f7aecfe6432
25Kanon <mjprofeta@tip.edu.ph>
2024-07-23 23:44:15 +0800

    Revert "initial commit"
    This reverts commit b54b3481cb61d127396f3e0fc723a51e1abea1b3.

--- front-synapse_space/src/ThemeContext.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-
-const ThemeContext = React.createContext({
-    theme: 'light', // Default theme
-    toggleTheme: () => { }, // Placeholder function
-});
-
-export default ThemeContext;
--- projectSynapseSpace/synapseSpace/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SynapsespaceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'synapseSpace'
