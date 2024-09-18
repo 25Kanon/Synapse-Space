ec9147abd214471c104bcab5d71b897884cbd57a
25Kanon <mjprofeta@tip.edu.ph>
2024-09-18 23:30:53 +0800

    url pattern adjusted

--- backend/ss_api/urls.py
+++ backend/ss_api/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from .views import RegisterView, LoginView, LogoutView, CustomTokenRefreshView
+from .views import RegisterView, LoginView, LogoutView, CustomTokenRefreshView, CommunityCreateView
 
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
+    # Auth URLs
+    path('auth/register/', RegisterView.as_view(), name='register'),
+    path('auth/login/', LoginView.as_view(), name='login'),
+    path('auth/logout/', LogoutView.as_view(), name='logout'),
+    path('auth/token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
 
+    # Community URLs
+    path('community/create/', CommunityCreateView.as_view(), name='community-create'),
 ]
