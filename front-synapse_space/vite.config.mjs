1e07f3041f2918888db300b8b452f87ca952cac4
25Kanon <mjprofeta@tip.edu.ph>
2024-10-24 01:16:40 +0800

    migrate from CRA to Vite

---
+++ front-synapse_space/vite.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    type: "module",
+    base: './',
+    plugins: [react()],
+    server: {
+        open: true,
+        port: 3000,
+        host: true,
+    },
+    build: {
+        outDir: 'build',
+    },
+});
