672dc0c41fd0f5e7673c3851b97c647166e64512
25Kanon <mjprofeta@tip.edu.ph>
2024-11-14 17:51:31 +0800

    change navigate to redirect; should force reset the callback memorization

--- front-synapse_space/src/components/community/LgCommunityPill.jsx
+++ front-synapse_space/src/components/community/LgCommunityPill.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import { useNavigate } from "react-router-dom";
+import { redirect } from "react-router-dom";
+
 import PropTypes from 'prop-types';
 
 const LgCommunityPill = ({ communityName, communityID, commAvatar}) => {
-    const navigate = useNavigate();
+
 
     const getInitials = (name) => {
         return name.split(' ').map(word => word[0]).join('');
     };
+    const handleRedirect = () => {
+        window.location.href = `/community/${communityID}`;
+    };
 
     return (
         <button
-            onClick={() => navigate(`/community/${communityID}`)}
+            onClick={handleRedirect}
             className="flex items-center w-full p-2 rounded-full group mt-3 hover:bg-gray-100 dark:hover:bg-gray-700 overflow-hidden whitespace-nowrap"
         >
             <div className="avatar placeholder">
