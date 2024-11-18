2df6837d9e4ad36b4a6cf09eb9d31fa0f3eb76fe
mesflorentino <eskaflorentino@gmail.com>
2024-11-19 01:35:16 +0800

    Profile Comments | Privacy Policy | Terms of Use

--- front-synapse_space/src/components/profile/NavBar.jsx
+++ front-synapse_space/src/components/profile/NavBar.jsx
@@ -10,7 +10,7 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import AuthContext from "../../context/AuthContext";
 import { useFriends } from "../../context/FriendContext";
-
+import { useNotifications } from "../../context/NotificationContext";
 const NavBar = () => {
     const navigate = useNavigate();
     const { user, logout } = useContext(AuthContext);
@@ -19,6 +19,8 @@ const NavBar = () => {
     const [scrolled, setScrolled] = useState(false);
     const [searchQuery, setSearchQuery] = useState(""); // State to hold search input
     const location = useLocation();
+    const { notifications, markAsRead } = useNotifications();
+    const unreadNotifications = notifications.filter((notification) => !notification.is_read); // Filter unread notifications
     const isOnSearchPage = location.pathname === "/search";
     const getInitials = (name) => {
         return name
@@ -67,8 +69,8 @@ const NavBar = () => {
                                 alt="FlowBite Logo"
                             />
                             <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
-                Synapse Space
-              </span>
+                                Synapse Space
+                            </span>
                         </a>
                     </div>
                     {/* Search form */}
@@ -88,17 +90,25 @@ const NavBar = () => {
 
                     <div className="flex items-center justify-center hidden px-3 my-auto rounded-full ms-3 bg-base-200 dropdown-left sm:block ">
                         <div className="z-40 flex items-center px-2 py-1">
-                            <FontAwesomeIcon icon={faMessage} className="z-40 h-5 mr-5" />
+
+                            <Link to="/messages" className="link">
+                                <FontAwesomeIcon icon={faMessage} className="z-40 h-5 mr-5" />
+                            </Link>
                             <details className="z-40 dropdown dropdown-end dropdown-bottom">
                                 <summary className="flex items-center h-5 btn">
                                     <FontAwesomeIcon icon={faBell} className="h-5" />
-                                    {filteredFriendRequests.length > 0 && (
-                                        <span className="absolute top-0 right-0 z-40 flex items-center justify-center w-5 h-5 text-xs text-white transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">
-                      {filteredFriendRequests.length}
-                    </span>
+                                    {(filteredFriendRequests.length > 0 || unreadNotifications.length > 0) && (
+                                        <span
+                                            className="absolute top-0 right-0 z-40 flex items-center justify-center w-5 h-5 text-xs text-white transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full">
+                                            {filteredFriendRequests.length + unreadNotifications.length}
+                                        </span>
                                     )}
                                 </summary>
                                 <ul className="menu dropdown-content bg-base-100 rounded-box z-[60] min-w-80 w-auto p-2 shadow">
+                                    {/* Friend Requests Section */}
+                                    <li className="menu-title">
+                                        <span>Friend Requests</span>
+                                    </li>
                                     {filteredFriendRequests.length > 0 ? (
                                         filteredFriendRequests.map((request) => (
                                             <li
@@ -115,10 +125,11 @@ const NavBar = () => {
                                                                     className="object-cover w-full h-full rounded-full"
                                                                 />
                                                             ) : (
-                                                                <div className="flex items-center justify-center w-full h-full bg-gray-300 rounded-full">
-                                  <span className="text-sm font-semibold">
-                                    {getInitials(request.sender_name)}
-                                  </span>
+                                                                <div
+                                                                    className="flex items-center justify-center w-full h-full bg-gray-300 rounded-full">
+                                                                    <span className="text-sm font-semibold">
+                                                                        {getInitials(request.sender_name)}
+                                                                    </span>
                                                                 </div>
                                                             )}
                                                         </div>
@@ -150,6 +161,84 @@ const NavBar = () => {
                                     ) : (
                                         <li className="p-2 text-center">No friend requests</li>
                                     )}
+
+                                    {/* Divider */}
+                                    <div className="divider"></div>
+
+                                    {/* Notifications Section */}
+                                    <li className="menu-title">
+                                        <span>Notifications</span>
+                                    </li>
+                                    {unreadNotifications.length > 0 ? (
+                                        unreadNotifications.map((notification) => (
+                                            <li
+                                                key={notification.id}
+                                                className="flex items-center justify-between w-full gap-3 p-2 whitespace-nowrap"
+                                                title={
+                                                    notification.message.action === "banned_from_community"
+                                                        ? `You have been banned from "${notification.message.community_name}".`
+                                                        : notification.message.action === "post_reported"
+                                                            ? `Your post in "${notification.message.community_name}" has been reported.`
+                                                            : notification.message.action === "comment_reported"
+                                                                ? `Your comment in "${notification.message.community_name}" has been reported.`
+                                                                : ""
+                                                } // Add detailed message here
+                                            >
+                                                <Link
+                                                    to={
+                                                        notification.message.action === "post_reported"
+                                                            ? `/community/${notification.message.community_id}/post/${notification.message.post_id}`
+                                                            : notification.message.action === "comment_reported"
+                                                                ? `/community/${notification.message.community_id}/post/${notification.message.post_id}#comment-${notification.message.comment_id}`
+                                                                : notification.message.action === "banned_from_community"
+                                                                    ? `/community/${notification.message.community_id}`
+                                                                    : "#"
+                                                    }
+                                                    className="text-sm font-medium truncate hover:underline"
+
+                                                    onClick={() => markAsRead(notification.id)}
+                                                >
+                                                    {/* Profile or Community Image */}
+                                                    <div className="flex-shrink-0">
+                                                        <div className="avatar placeholder">
+                                                            <div className="w-10 h-10 rounded-full">
+                                                                {notification.message?.friend?.profile_pic ? (
+                                                                    <img
+                                                                        src={notification.message.friend.profile_pic}
+                                                                        alt="User"
+                                                                        className="object-cover w-full h-full rounded-full"
+                                                                    />
+                                                                ) : notification.community_imgURL ? (
+                                                                    <img
+                                                                        src={notification.community_imgURL}
+                                                                        alt="Community"
+                                                                        className="object-cover w-full h-full rounded-full"
+                                                                    />
+                                                                ) : (
+                                                                    <div className="flex items-center justify-center w-full h-full bg-gray-300 rounded-full">
+                                                                        <span className="text-sm font-semibold">N/A</span>
+                                                                    </div>
+                                                                )}
+                                                            </div>
+                                                        </div>
+                                                    </div>
+
+                                                    {/* Notification Title and Details */}
+                                                    <div className="flex-1 min-w-0">
+                                                        {notification.title}
+                                                    </div>
+                                                </Link>
+                                                {/* <button
+                                                    onClick={() => markAsRead(notification.id)}
+                                                    className="btn btn-xs btn-outline"
+                                                >
+                                                    Mark as Read
+                                                </button> */}
+                                            </li>
+                                        ))
+                                    ) : (
+                                        <li className="p-2 text-center">No notifications</li>
+                                    )}
                                 </ul>
                             </details>
 
@@ -164,10 +253,11 @@ const NavBar = () => {
                                                     className="object-cover w-full h-full rounded-full"
                                                 />
                                             ) : (
-                                                <div className="flex items-center justify-center w-full h-full bg-gray-300 rounded-full">
-                          <span className="text-sm font-semibold">
-                            {getInitials(user.username)}
-                          </span>
+                                                <div
+                                                    className="flex items-center justify-center w-full h-full bg-gray-300 rounded-full">
+                                                    <span className="text-sm font-semibold">
+                                                        {getInitials(user.username)}
+                                                    </span>
                                                 </div>
                                             )}
                                         </div>
@@ -175,8 +265,8 @@ const NavBar = () => {
                                     <p className="flex items-center text-sm font-semibold">
                                         {user.username}
                                         <span>
-                      <FontAwesomeIcon icon={faChevronDown} className="ms-3" />
-                    </span>
+                                            <FontAwesomeIcon icon={faChevronDown} className="ms-3" />
+                                        </span>
                                     </p>
                                 </summary>
                                 <ul className="menu dropdown-content bg-base-100 rounded-box z-[50] w-52 p-2 shadow">
