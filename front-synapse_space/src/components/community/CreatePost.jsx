aad53d49938358c81ad7b9133bb6c6a1faadca66
25Kanon <mjprofeta@tip.edu.ph>
2024-11-23 02:36:34 +0800

    block posts with profanity upon posting

--- front-synapse_space/src/components/community/CreatePost.jsx
+++ front-synapse_space/src/components/community/CreatePost.jsx
@@ -1,21 +1,56 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import DOMPurify from "dompurify";
 import axios from "axios";
+import Swal from "sweetalert2";
 import ErrorAlert from "../ErrorAlert";
 import SuccessAlert from "../SuccessAlert";
 import PropTypes from 'prop-types';
 import '@mdxeditor/editor/style.css';
 import RichTextEditor from "../RichTextEditor";
 import AxiosInstance from '../../utils/AxiosInstance';
+import Loading from "../Loading";
 
-const API_URL = import.meta.env.VITE_API_BASE_URI;
-const CreatePost = ({ userName, community, onPostCreated }) => {
+const CreatePost = ({ userName, community, onPostCreated, rules }) => {
     const [success, setSuccess] = useState(null);
     const [error, setError] = useState(null);
     const [title, setTitle] = useState("");
     const [isDarkMode, setIsDarkMode] = useState(false);
     const [editorContent, setEditorContent] = useState("");
     const [isFormVisible, setIsFormVisible] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const dialogRef = useRef(null);
+
+    const editorJsToPlainText = (editorContent) => {
+        if (!editorContent || !Array.isArray(editorContent.blocks)) {
+            console.log('Invalid editor content');
+            return '';
+        }
+
+        console.log('Processing blocks:', editorContent.blocks);
+
+        return editorContent.blocks
+            .map(block => {
+                if (!block || !block.data) return '';
+
+                console.log('Block Data:', block.data);
+                switch (block.type) {
+                    case 'paragraph':
+                        return block.data.text || '';
+                    case 'header':
+                        return block.data.text || '';
+                    case 'list':
+                        return block.data.items.join('\n');
+                    case 'quote':
+                        return block.data.text || '';
+                    case 'image':
+                        return '';
+                    default:
+                        return '';
+                }
+            })
+            .join('\n')
+            .trim();
+    };
 
     useEffect(() => {
         const currentTheme = document.documentElement.getAttribute('data-theme');
@@ -24,6 +59,25 @@ const CreatePost = ({ userName, community, onPostCreated }) => {
 
     const toggleFormVisibility = () => {
         setIsFormVisible(!isFormVisible);
+    };
+
+    const profanityCheck = async (title, content) => {
+        try {
+            const payload = {
+                title: DOMPurify.sanitize(title),
+                content: DOMPurify.sanitize(content),
+            };
+
+            const response = await AxiosInstance.post('/api/check_profanity/', payload, {
+                withCredentials: true,
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            });
+            return response.data.allow;
+        } catch (error) {
+            return false;
+        }
     };
 
     const handlePostClick = async () => {
@@ -40,15 +94,29 @@ const CreatePost = ({ userName, community, onPostCreated }) => {
         }
 
         try {
-            // Move images to permanent storage and update URLs
+            setLoading(true);
             const updatedContent = await moveImagesToPermanentStorage(editorContent);
+
+            const plainText = editorJsToPlainText(updatedContent);
+            const hasProfanity = await profanityCheck(title, plainText);
+
+            if (!hasProfanity) {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Post contains profanity!',
+                });
+                setLoading(false);
+                return;
+            }
 
             const formData = new FormData();
             formData.append("title", DOMPurify.sanitize(title));
             formData.append("content", JSON.stringify(updatedContent));
             formData.append("posted_in", community);
-                
-            const response = await AxiosInstance.post(`/api/community/${community}/post`, formData, { withCredentials: true,
+
+            const response = await AxiosInstance.post(`/api/community/${community}/post`, formData, {
+                withCredentials: true,
                 headers: {
                     'Content-Type': 'multipart/form-data',
                 }
@@ -59,12 +127,16 @@ const CreatePost = ({ userName, community, onPostCreated }) => {
                 setEditorContent('');
                 setSuccess('Post submitted successfully');
                 toggleFormVisibility();
-                onPostCreated(); // Trigger the fetchPosts useEffect
+                onPostCreated();
             } else {
-                setError('Error submitting post' + response.statusText);
+                setError('Error submitting post: ' + response.statusText);
             }
         } catch (error) {
-            setError('Error submitting post' + error);
+            setError('Error submitting post: ' + error.message);
+            console.error('Post submission error:', error);
+        } finally {
+            setLoading(false);
+            document.getElementById('loading-modal').close();
         }
     };
 
@@ -72,7 +144,8 @@ const CreatePost = ({ userName, community, onPostCreated }) => {
         const updatedBlocks = await Promise.all(content.blocks.map(async (block) => {
             if (block.type === 'image') {
                 const tempUrl = block.data.file.url;
-                const response = await AxiosInstance.post(`/api/move-image/`, { tempUrl }, { withCredentials: true,
+                const response = await AxiosInstance.post(`/api/move-image/`, { tempUrl }, {
+                    withCredentials: true,
                     headers: {
                         'Content-Type': 'multipart/form-data',
                     }
@@ -86,10 +159,27 @@ const CreatePost = ({ userName, community, onPostCreated }) => {
         return { ...content, blocks: updatedBlocks };
     };
 
+    useEffect(() => {
+        if (loading) {
+            document.getElementById('loading-modal').showModal();
+        }
+    }, [loading]);
+
     return (
         <>
-            {error && <ErrorAlert text={error}/> }
-            {success && <SuccessAlert text={success} />}
+            {error && <ErrorAlert text={error}/>}
+            {success && <SuccessAlert text={success}/>}
+            <dialog
+                ref={dialogRef}
+                id="loading-modal"
+                className="modal modal-bottom sm:modal-middle"
+                onClose={(e) => e.preventDefault()}
+                onCancel={(e) => e.preventDefault()}
+            >
+                <form method="dialog" className="modal-box" onSubmit={(e) => e.preventDefault()}>
+                    <Loading loadingText="Please wait..."/>
+                </form>
+            </dialog>
 
             <label className="flex items-center gap-2 input input-bordered">
                 <button type="button" className="grow text-start" onClick={toggleFormVisibility}>
@@ -99,12 +189,14 @@ const CreatePost = ({ userName, community, onPostCreated }) => {
             {isFormVisible && (
                 <form className="p-2 m-5 rounded bg-base-100">
                     <div className="mb-5">
-                        <label htmlFor="title" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
-                        <input required={true} type="text" id="title" className="w-full input input-bordered" value={title} onChange={(e) => setTitle(e.target.value)} />
+                        <label htmlFor="title"
+                               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
+                        <input required={true} type="text" id="title" className="w-full input input-bordered"
+                               value={title} onChange={(e) => setTitle(e.target.value)}/>
                     </div>
                     <span className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Body</span>
                     <div className="mb-5">
-                        <RichTextEditor setEditorContent={setEditorContent} isEditing={false} />
+                        <RichTextEditor setEditorContent={setEditorContent} isEditing={false}/>
                     </div>
                     <button type="button" className="btn btn-primary" onClick={handlePostClick}>Post</button>
                 </form>
