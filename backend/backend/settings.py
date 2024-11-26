45a14b1a6c38bad510c1f5c97eea7f6a20089b10
25Kanon <mjprofeta@tip.edu.ph>
2024-11-26 23:23:05 +0800

    configure and collect static files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -273,8 +273,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+DEBUG = os.getenv('DEBUG', 'True')
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
