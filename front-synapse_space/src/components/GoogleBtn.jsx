479bd08ab87e44d9eed501ef5b62991485d9c681
25Kanon <mjprofeta@tip.edu.ph>
2024-11-27 19:15:46 +0800

    show error for google login

--- front-synapse_space/src/components/GoogleBtn.jsx
+++ front-synapse_space/src/components/GoogleBtn.jsx
@@ -1,12 +1,16 @@
 import { GoogleLogin, GoogleOAuthProvider, useGoogleLogin } from "@react-oauth/google";
 import AxiosInstance from "../utils/AxiosInstance";
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faGoogle} from "@fortawesome/free-brands-svg-icons";
 import AuthContext from '../context/AuthContext';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import ErrorAlert from "./ErrorAlert";
 
 export default function GoogleBtn() {
     const { isLoggedinWithGoogle } = useContext(AuthContext);
+    const [error, setError] = useState(null);
     const login = useGoogleLogin({
         onSuccess: async (response) => {
             try {
@@ -18,10 +22,13 @@ export default function GoogleBtn() {
                 await isLoggedinWithGoogle()
             } catch (error) {
                 const errorMessage = error.response?.data?.non_field_errors || 'Google login failed';
+                setError(errorMessage);
                 console.error('Google login failed:', errorMessage);
+
             }
         },
         onError: (error) => {
+            setError(error);
             console.error('Google Login Error:', error);
         },
         scope: 'openid profile email',
@@ -29,12 +36,14 @@ export default function GoogleBtn() {
     });
 
     return (
-        <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-            <button className="btn btn-primary  text-center w-full" onClick={login}>
-                <FontAwesomeIcon icon={faGoogle}/>
-                Login with Google
-            </button>
-
-        </GoogleOAuthProvider>
+        <>
+            {error && <ErrorAlert text={error} />}
+            <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+                <button className="btn btn-primary text-center w-full" onClick={login}>
+                    <FontAwesomeIcon icon={faGoogle} />
+                    Login with Google
+                </button>
+            </GoogleOAuthProvider>
+        </>
     );
 }
